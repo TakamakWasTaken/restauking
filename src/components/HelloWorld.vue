b563223ac4696219a439c16c2e29c2b3c46f58a9
TakamakWasTaken <gau.n@hotmail.fr>
2021-04-10 17:12:12 +0200

    Resto card list completed

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,16 +1,14 @@
 <template>
   <v-container>
    
-    <v-layout v-if="this.restos != undefined" align-center justify-center row fill-height>
+    <v-layout v-if="restos != undefined" align-center justify-center row fill-height>
       <v-flex
         v-for="r in restos"
         :key="r.id"
         xs4
         sm3
-        class="flexContainer"
-      >
-        <v-card width="95%" height="380px" class="restoCard">
-          
+        class="restoCardContainer">
+        <v-card class="restoCard">
           <v-img v-if="r.image_url"
             :src="`${ r.image_url }`"
             class="restaurantImg"
@@ -19,12 +17,34 @@
             :src="require('../assets/restaurant-default.png')"
             class="restaurantImg"
           ></v-img>
-          <v-card-title primary-title>
-            <h2 class="headline mb-0">{{ r.name }}</h2>
-          </v-card-title>
-          <div class="restaurantLocation">{{ r.location.city }}
+          <div class="restaurantInfos">
+            <v-card-title primary-title>
+              <h2 class="headline mb-0">{{ r.name }}</h2>
+            </v-card-title>
+            <div class="restaurantLocation">
+              <img :src="require('../assets/position.png')" class="logoPosition" height="21px">
+              {{ r.location.city }}<div v-if="r.price" style="margin-left: 5px;"> - {{ r.price}}</div>
+            </div>
+            <v-layout class="categoryContainer" v-if="r.categories.length != 0">
+              <v-card class="restaurantCategory" v-for="c in r.categories" :key="c.id">{{ c.title }}</v-card>
+
+            </v-layout>
+            <div v-else>no cat</div>
+            <v-rating
+              background-color="green lighten-2"
+              color="green"
+              half-increments
+              hover
+              readonly
+              length="5"
+              size="24"
+              :value="r.rating"
+            ></v-rating>
+            <v-btn color="secondary" small elevation="2">
+              Voir le détail
+            </v-btn>
           </div>
-        <v-card></v-card>
+          
         </v-card>
       </v-flex>
     </v-layout>
@@ -35,14 +55,20 @@
 <script>
   export default {
     name: 'HelloWorld',
+    computed: {
 
+    },
     data: () => ({
       restos: undefined
     }),
-   mounted: function () {
-     this.$store.dispatch('getRestaurantsAsync');
-     this.$store.dispatch('getRestaurantsAsync',{search:'',open_now:true,categories:'FastFood'});
-     this.$store.dispatch('getDetailsRestaurantAsync','4qS4kIbGlGfswmUY-o37_g');
+    mounted: function () {
+      var store = this.$store;
+      // store.dispatch('getRestaurantsAsync');
+      // store.dispatch('getRestaurantsAsync',{search:'',open_now:true,categories:'FastFood'});
+      // store.dispatch('getDetailsRestaurantAsync','4qS4kIbGlGfswmUY-o37_g');
+      store.dispatch('getRestaurantsAsync',{location:'Clermont-Ferrand',search:'',is_closed:true,categories:'Fast Food'}).then(() => {
+       this.restos = store.state.restaurant.restaurants;
+     });
     }
   }
 </script>
